f682e0664ec15ac20d3dea0f1844884a52b8afee
inozemtsev-ivan <inozemtsev.il@gmail.com>
2017-11-20 20:09:15 +0300

    Basic logic was implemented.

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,23 @@
+import os
+import sys
 import json
 
 
 def load_data(filepath):
-    pass
+    try:
+        full_path = os.path.join(os.path.curdir, filepath)
+        with open(full_path, 'r') as json_file:
+            json_data = json_file.read()
+            return json.loads(json_data, encoding='UTF-8')
+    except FileExistsError:
+        print('File does not exist!')
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, ensure_ascii=False, indent=4))
 
 
 if __name__ == '__main__':
-    pass
+    loaded_data = load_data(sys.argv[1])
+    if loaded_data:
+        pretty_print_json(loaded_data)
